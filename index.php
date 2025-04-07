04af4953660ba5f5e6932189125786ea9ed732ad
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-04-07 09:30:08 -0500

    Eliminar archivos

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'Usuario.php';
-
-$usuario = new Usuario();
-
-// Aquí puedes probar:
-// $usuario->crearUsuario("Ana", "ana@mail.com", 22);
-// print_r($usuario->listarUsuarios());
