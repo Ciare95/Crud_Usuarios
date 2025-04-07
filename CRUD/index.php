f760ce8c150c884294890e718a140926eadf6bca
jhon deivid rojas rodriguez <jhonrojasr253@gmail.com>
2025-04-07 10:45:56 -0500

    arreglar creacion de usuarios por parametros incorrectos

--- CRUD/index.php
+++ CRUD/index.php
@@ -5,5 +5,8 @@ require_once 'Usuario.php';
 $usuario = new Usuario();
 
 // Aquí puedes probar:
-// $usuario->crearUsuario("Ana", "ana@mail.com", 22);
+$usuario->crearUsuario('Jhon', 'Deivid', 
+                        'Rojas', 'Rodriguez', 
+                        '2003-01-05', '315789456', 
+                        'nose@gmail.com', 'cra 17 12-23');
 // print_r($usuario->listarUsuarios());
